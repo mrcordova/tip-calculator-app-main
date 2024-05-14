e3ffe443e71da1f3a95faf9dfd8b40a0d2758ae7
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-13 21:03:37 -0700

    add code to toggle reset btn

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@ const billInput = document.getElementById("bill");
 const tipRadioBtns = document.querySelectorAll('.tips-con>div');
 const customTipInput = document.getElementById('custom');
 const peopleInput = document.getElementById('people');
+const resetBtn = document.getElementById('resetBtn');
 
 let bill = 0;
 let tipPercentage = 0.0;
 let numOfPeople = 0;
 
+
 const addTipPercentage = (e) => {
     e.preventDefault()
+    changeResetBtnState(false);
     tipPercentage = e.currentTarget.children[0].value;
     console.log(tipPercentage);
 }
 
 const converToPercentage = (num) => (num / 100);
 
-// console.log(tipRadioBtns);
+const changeResetBtnState = (val) => resetBtn.disabled = val;
+
 
 billInput.addEventListener("input", (e) => {
-    // console.log("input changed", e.currentTarget.value);
+    changeResetBtnState(false);
     bill = e.currentTarget.value;
 })
 
@@ -27,22 +31,22 @@ for (tipRadioBtn of tipRadioBtns) {
 }
 
 customTipInput.addEventListener("input", (e) => {
+   
     tipPercentage = converToPercentage(e.currentTarget.value);
     console.log(tipPercentage);
+    
 });
 
 peopleInput.addEventListener("input", (e) => {
     const peopleLab = document.querySelector('label[for=people]>span');
-    // if (!e.currentTarget.validality) {
-    //     peopleLab.classList.remove('hide');
-    // } else {
-    //     peopleLab.classList.add('hide');
-    // }
+  
     numOfPeople = e.currentTarget.value === "" ? 0 : Math.floor(e.currentTarget.value);
     if (numOfPeople == 0 && peopleLab.classList.contains('hide') ) {
         peopleLab.classList.remove('hide');
         peopleInput.classList.add('invalid');
+        changeResetBtnState(true);
     } else if (numOfPeople !== 0) {
+        changeResetBtnState(false);
         peopleLab.classList.add('hide');
         peopleInput.classList.remove('invalid');
     }
