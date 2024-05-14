d99295b04022ec193823579b3d6271a3f7b02dac
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-13 19:25:48 -0700

    code for adding tip

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const billInput = document.getElementById("bill");
+const tipRadioBtns = document.querySelectorAll('.tips-con>div');
+let bill = 0;
+let tipPercentage = 0.0;
+
+// console.log(tipRadioBtns);
 
 billInput.addEventListener("input", (e) => {
-    console.log("input changed", e.currentTarget.value);
+    // console.log("input changed", e.currentTarget.value);
+    bill = e.currentTarget.value;
 })
+
+
+const addTipPercentage = (e) => {
+    e.preventDefault()
+    tipPercentage = e.currentTarget.children[0].value;
+    console.log(tipPercentage);
+}
+for (tipRadioBtn of tipRadioBtns) {
+    tipRadioBtn.addEventListener("click", addTipPercentage);
+}
