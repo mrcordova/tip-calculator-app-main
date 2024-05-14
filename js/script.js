81ce8149e3426ce5d8e80ea41010ab33106218d8
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-13 20:41:21 -0700

    add code for people input and change class specifiy for people input
    I used #people to put it on the same level as the pseudo class hover

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const billInput = document.getElementById("bill");
 const tipRadioBtns = document.querySelectorAll('.tips-con>div');
 const customTipInput = document.getElementById('custom');
+const peopleInput = document.getElementById('people');
 
 let bill = 0;
 let tipPercentage = 0.0;
+let numOfPeople = 0;
 
 const addTipPercentage = (e) => {
     e.preventDefault()
@@ -28,3 +30,21 @@ customTipInput.addEventListener("input", (e) => {
     tipPercentage = converToPercentage(e.currentTarget.value);
     console.log(tipPercentage);
 });
+
+peopleInput.addEventListener("input", (e) => {
+    const peopleLab = document.querySelector('label[for=people]>span');
+    // if (!e.currentTarget.validality) {
+    //     peopleLab.classList.remove('hide');
+    // } else {
+    //     peopleLab.classList.add('hide');
+    // }
+    numOfPeople = e.currentTarget.value === "" ? 0 : Math.floor(e.currentTarget.value);
+    if (numOfPeople == 0 && peopleLab.classList.contains('hide') ) {
+        peopleLab.classList.remove('hide');
+        peopleInput.classList.add('invalid');
+    } else if (numOfPeople !== 0) {
+        peopleLab.classList.add('hide');
+        peopleInput.classList.remove('invalid');
+    }
+    console.log(numOfPeople);
+})
